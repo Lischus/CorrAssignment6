9317b5b17fc7e1a99017282ac00508549bed35de
Martin Corr <martinjcorr2@gmail.com>
2022-01-14 20:05:31 -0500

    buttons now appear after you select a city

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // function getApi() {
 //     // fetch request gets a list of all the repos for the node.js organization
 //     var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
-  
+
 //     fetch(requestUrl)
 //       .then(function (response) {
 //         return response.json();
@@ -14,11 +14,11 @@
 //           var createTableRow = document.createElement('tr');
 //           var tableData = document.createElement('td');
 //           var link = document.createElement('a');
-  
+
 //           // Setting the text of link and the href of the link
 //           link.textContent = data[i].html_url;
 //           link.href = data[i].html_url;
-  
+
 //           // Appending the link to the tabledata and then appending the tabledata to the tablerow
 //           // The tablerow then gets appended to the tablebody
 //           tableData.appendChild(link);
@@ -27,17 +27,48 @@
 //         }
 //       });
 //   }
-function getApi() {
-var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=61c4e10047acc72ea5e22c4f2a7d9dac"
 
-fetch(requestUrl)
-    .then(function (response) {
-        console.log("fetch the URL, please")
-        return response.json();
-    })
-}
+var searchedCities = []
 
 var searchButton = document.querySelector(".searchBtn")
+
+
+function getCoordinates(city) {
+    console.log(city)
+    var coordinateURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=61c4e10047acc72ea5e22c4f2a7d9dac`
+    fetch(coordinateURL)
+        .then(function (response) {
+            console.log("fetch the URL, please")
+            return response.json();
+        }).then(function (data) {
+            console.log(data)
+            getApi(city, data.coord.lat, data.coord.lon)
+        })
+}
+
+var todaysForecast = document.querySelector(".today")
+
+var fiveDayLocation = document.querySelector(".five-day")
+
+function getApi(city, lat, lon) {
+    var requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=61c4e10047acc72ea5e22c4f2a7d9dac`
+
+    fetch(requestURL)
+        .then(function (response) {
+            console.log("fetch the URL, please")
+            return response.json();
+        }).then(function (data) {
+            console.log(data)
+            var iconURL = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`
+            console.log(iconURL)
+            for (var i = 0; i <= 5; i++) {
+                var fiveDayIcon = document.createElement("img")
+                fiveDayIcon.setAttribute("class", "fiveDayWeatherIcon")
+                
+            }
+            //use data.current for the current weather, then for loop through the data.daily for each of the five cards (don't for loop length, just for loop 5 times) when you want to put
+        })
+}
 
 var searchBar = document.querySelector(".searchBar")
 
@@ -47,11 +78,32 @@ var pastCities = document.querySelector(".pastCities")
 
 pastCities.textContent = '';
 
-function beginSearch () {
+function beginSearch() {
     var currentCity = $(searchRow).children(".searchBar").val()
     console.log("You sure clicked that Search Button, big guy");
-    localStorage.setItem(searchRow, currentCity);
-    $(".searchBar").val(localStorage.getItem("[object HTMLDivElement]"))
+    searchedCities.push(currentCity)
+    localStorage.setItem("City History", searchedCities);
+    cityButtons(searchedCities)
+    getCoordinates(currentCity)
+}
+
+function cityButtons(cities) {
+    pastCities.innerHTML = ""
+    for (var i = 0; i < cities.length; i++) {
+        var cityButton = document.createElement("button")
+        cityButton.setAttribute("class", "buttonHistory");
+        cityButton.setAttribute("data-search", cities[i]);
+        cityButton.textContent = cities[i]
+        pastCities.append(cityButton)
+    }
+}
+
+function handleSearchHistory(event) {
+    var historyButton = event.target
+    var clickedCity = historyButton.getAttribute("data-search");
+    getCoordinates(clickedCity)
 }
 
 searchButton.addEventListener("click", beginSearch)
+
+pastCities.addEventListener("click", handleSearchHistory)
